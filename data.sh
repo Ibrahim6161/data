7c97784192e595d5d2b81d310ce3ef2c21da0c48
Ibrahim6161 <43389544+Ibrahim6161@users.noreply.github.com>
2020-04-04 14:26:06 +0200

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,4 +1,9 @@
-#!/bin/sh
+#!/bin/bash
+
+yum install -y yum-utils device-mapper-persistent-data lvm2
+yum-config-manager -y --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum install -y docker-ce docker-ce-cli containerd.io
+
 cat >/etc/yum.repos.d/mongodb-org-4.2.repo <<EOL
 [mongodb-org-4.2]
 name=MongoDB Repository
@@ -7,6 +12,10 @@ gpgcheck=1
 enabled=1
 gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc
 EOL
+
 sudo yum install -y mongodb-org
+
 sudo systemctl enable mongod
 sudo systemctl start mongod
+sudo systemctl enable docker
+sudo systemctl start docker
